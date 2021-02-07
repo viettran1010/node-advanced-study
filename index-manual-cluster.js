d4a08686737195a402d68d57a3cbf3a505832cba
Viet <48788462+viettran1010@users.noreply.github.com>
2021-02-07 20:02:59 +0700

    Update index-manual-cluster.js

--- index-manual-cluster.js
+++ index-manual-cluster.js
@@ -1,4 +1,5 @@
-process.env.UV_THREADPOOL_SIZE = 1;
+// We are going to create 2 child here in cluster mode
+process.env.UV_THREADPOOL_SIZE = 1; // Set pool size of theadpool to 1 here (default is 4). So this application is running in single thread mode
 const express = require('express')
 const app = express();
 const crypto = require('crypto');
@@ -7,32 +8,23 @@ const cluster = require('cluster')
 // Is the file being run in master mode?
 if (cluster.isMaster) {
     // Cause index.js to be execute again but in child mode
-    cluster.fork();
+    cluster.fork(); // Create a child
     cluster.fork();
 
     // cluster.fork();
     // cluster.fork();
 }
-else {
-    // Im a child, I'm going to act like a server and do nothing else
-    // const doWork = (duration)=> {
-    //     const start = Date.now();
-
-    //     while (Date.now()-start < duration) {
-
-    //     }
-    // }
-
+else {    
     app.get('/', (req, res)=> {
-        // doWork(5000);
-        crypto.pbkdf2('a', 'b', 200000, 512, 'sha512', ()=> {
+        crypto.pbkdf2('a', 'b', 200000, 512, 'sha512', ()=> { // Run some time consuming taksk .ie crypto
             // console.log('1: ', Date.now() - start);
-            res.send('Hi')
+            res.send('Hi') // After execution done, return some message
         })
         
     })
 
-    app.get('/fast', (req,res)=> {
+    app.get('/fast', (req,res)=> { // This 'fast' api is for the purpose of showing that when server is busy with other task and threadpool size is 1, any
+                                   // response that's normally supposed to be quick, is delayed by the foregoing time consuming request
         res.send('This was fast!')
     })
 
